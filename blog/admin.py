c2c09d2f9fc1adb7828d6bfe8cddd5b806e0859b
Tahir Altundağ <tahiraltundag1@gmail.com>
2023-07-30 23:24:58 +0300

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from blog.models import Post, Category, Comments
+# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publishing_date', 'slug', 'status']
+    list_display_links = ['publishing_date']
+    list_filter = ['publishing_date']
+    search_fields = ['title', 'content']
+    list_editable = ['title']
+
+    class Meta:
+        model = Post
+
+class CategoryAdmin(admin.ModelAdmin):
+    #öncelikli olarak görünecek alan
+    prepopulated_fields = {"slug": ("title",)}
+    #görünecek alanları belirliyoruz
+    list_display = (
+        "pk",
+        "title",
+        "slug",
+    )
+
+class CommentAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Comments
+        
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comments, CommentAdmin)
+
+
+
+
+
+
